b11d556a8734661218180ef4308ea74fd54cee32
Harsh Kishor <harshkishor2002@gmail.com>
2021-12-29 23:14:37 +0530

    form out

--- app.js
+++ app.js
@@ -25,13 +25,6 @@ app.post("/",(req,res)=>{
 })
 
 
-
-
-
-
-
-
-
 app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function() {
     console.log('Server started on port '+app.get('port'));
